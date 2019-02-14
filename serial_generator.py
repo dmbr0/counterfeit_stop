a1491e1c8bf53401dff84d3711a0af053a814d03
alejandro <alejandro@appbot.net9s.pw>
2019-02-14 18:13:16 -0500

    changed serial number length

--- serial_generator.py
+++ serial_generator.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 import strgen 
 
-print(strgen.StringGenerator("[\d\w]{10}").render_list(20,unique=True))
+
+print(strgen.StringGenerator("[\d\w]{7}").render_list(20,unique=True))

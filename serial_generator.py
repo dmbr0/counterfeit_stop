0be1d3a43c62f1ebdc18bee0a622115e1735de7d
alejandro <alejandro@appbot.net9s.pw>
2019-02-14 23:23:38 -0500

    ask user for batch number

--- serial_generator.py
+++ serial_generator.py
@@ -2,4 +2,5 @@
 import strgen 
 batch_num = input("Please enter a batch ID:")
 serialqty = input("How many serial numbers would you like to create?:")
+print("Batch:", batch_num)
 print(strgen.StringGenerator("[\d\w]{7}").render_list(serialqty,unique=True))

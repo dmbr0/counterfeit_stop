88942a38e060c422a1b0f48d07719a95cbba0f41
alejandro <alejandro@appbot.net9s.pw>
2019-02-16 13:19:55 -0500

    generate password protect a list of bip38 private keys with serial numbers

--- serial_generator.py
+++ serial_generator.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
-import strgen 
+import strgen
+from bip38 import *
+from bitcoin import *
+
 batch_num = input("Please enter a batch ID:")
-serialqty = input("How many serial numbers would you like to create?:")
-print("Batch:", batch_num)
-print(strgen.StringGenerator("[\d\w]{7}").render_list(serialqty,unique=True))
+serialqty = input("How many products would you like to mark?:")
+
+serials = strgen.StringGenerator("[\d\w]{7}").render_list(serialqty,unique=True)
+
+for serial in serials:
+    serial = serial.encode('utf-8')
+    pasw = serial
+    priv = random_key()
+    priv2 = decode_privkey(priv,'hex')
+    wif = encode_privkey(priv, 'wif')
+    addr = privtoaddr(priv)
+    bip = bip38_encrypt(priv,pasw)
+    print(bip,serial)

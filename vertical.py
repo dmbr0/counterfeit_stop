bae8cd13dd1ebf20c162e246f65b49b0fe66a132
Chris Gliatas <40585137+Cgliatas@users.noreply.github.com>
2019-03-10 18:55:27 -0400

    Update vertical.py

--- vertical.py
+++ vertical.py
@@ -1,16 +1,7 @@
 import numpy as np
 from PIL import Image
 # need to put the .jpg files in one by one.  Want to find a way to get everthing in a spacific directory.
-images_list = ['/Users/christophergliatas/projects/counterfeit_stop/1/1HrvvBZGg5hGDx3rKDbPHyU2pzH2u1nQcK.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/1HKGbK6dTMuaTxmDMzHPWJvctCoyB1mJ3G.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/1DZLhuHQ95SobrHQmyfyUM4Dbc4BGPJobb.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/1HLmkRSdG1Ewf79HsNj9Mt2dnQ6qi9CNWN.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/1KZfd6rH62uycCMQuXbnjUzSMqB27SYVFN.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/1NXv5re2pm6yuv6fv8AwomCFUqfbkKvJF7.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/1Q7oHd7JjWZgkJua1f2P9QKdXFB1VZTbcY.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/16oBXr6ETrsT41M4D8PmwPds8azY71cgL3.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/18UA3JQ7gvY18M5F6iz1cN4webiJmSXk79.jpg',
-    '/Users/christophergliatas/projects/counterfeit_stop/1/121zR3yzQaP2VvGvvDRAXD2RyAuBG17MkX.jpg',]
+images_list = [#path to .jpg]
 imgs = [ Image.open(i) for i in images_list ]
 # Take the size of the smallest .jpg
 min_img_shape = sorted( [(np.sum(i.size), i.size ) for i in imgs])[0][1]
